20a830d89c96f93c32720c66bfc57b22dbe08b6e
Dhruv Akbari <dhruvakbari@sassyinfotech.com>
2024-01-10 18:17:24 +0530

    astra and create plugin file with setting

---
+++ wp-content/plugins/CPTUI/template-business-terms.php
@@ -0,0 +1,13 @@
+<?php
+/*Template Name: News*/
+get_header();
+query_posts(array(
+    'post_type' => 'business-term',
+)); ?>
+<?php
+while (have_posts()) : the_post(); ?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+<!--    <p>--><?php //the_excerpt(); ?><!--</p>-->
+<?php endwhile;
+get_footer();
+?>
